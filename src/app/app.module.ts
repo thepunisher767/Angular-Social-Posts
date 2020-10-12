35f95c43967fa41639dac1f013fb1651f23559bf
zscholts <zachscholts@quickenloans.com>
2020-10-12 19:34:53 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { FormsModule } from '@angular/forms';
+import { SocialPostsComponent } from './social-posts/social-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostFormComponent,
+    SocialPostsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
